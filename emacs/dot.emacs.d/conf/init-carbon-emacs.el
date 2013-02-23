ac7fedc6a82ad8b75da74f19ade2d85578edb723
akitok <akito.dev@gmail.com>
2013-02-23 18:20:37 +0900

    Migrate from Carbon Emacs to Cocoa Emacs

--- emacs/dot.emacs.d/conf/init-carbon-emacs.el
+++ emacs/dot.emacs.d/conf/init-carbon-emacs.el
@@ -2,11 +2,6 @@
 
 
 ;;-----------------------------------------------------------------
-;; ファイル名の濁点・半濁点を化けないようにする
-;;-----------------------------------------------------------------
-(autoload 'utf-8m "utf-8m" t)
-(set-file-name-coding-system 'utf-8m)
-;;-----------------------------------------------------------------
 ;; Window size and position
 ;;-----------------------------------------------------------------
 (setq initial-frame-alist '((width . 100) (height . 50)))
@@ -14,62 +9,6 @@
 ;; 半透明
 ;;-----------------------------------------------------------------
 (add-to-list 'default-frame-alist '(alpha . (0.97 0.9)))
-;;-----------------------------------------------------------------
-;; Font
-;;-----------------------------------------------------------------
-(require 'carbon-font)  
-;; profont + hiragino maru gothic
-;;(setq carbon-font-encode-family-list-profont
-;;      '((ascii . "profontwindows")
-;;	  (japanese-jisx0208 . "ヒラギノ丸ゴ*")
-;;          (katakana-jisx0201 . "ヒラギノ丸ゴ*")
-;;          (korean-ksc5601 . "applegothic*")))
-;;  (carbon-font-create-fontset "profontmaru"
-			      ;; carbon-font-defined-sizes
-;;                              10
-;;			      carbon-font-encode-family-list-profont)
-  
-;; verdana + hiragino maru gothic
-;;(setq carbon-font-encode-family-list-verdana
-;;	'((ascii . "verdana")
-;;        (japanese-jisx0208 . "ヒラギノ丸ゴ*")
-;;        (katakana-jisx0201 . "ヒラギノ丸ゴ*")
-;;        (korean-ksc5601 . "applegothic*")))
-;;(carbon-font-create-fontset "verdanamaru"
-			      ;; carbon-font-defined-sizes
-;;                            11
-;;			      carbon-font-encode-family-list-verdana)
-  
-;; vera + hiragino maru gothic
-(setq carbon-font-encode-family-list-vera
-      '((ascii . "Bitstream Vera Sans Mono*")
-	(japanese-jisx0208 . "ヒラギノ丸ゴ*")
-	(katakana-jisx0201 . "ヒラギノ丸ゴ*")
-	(korean-ksc5601 . "applegothic*")))
-(carbon-font-create-fontset "veramaru"
-			    ;; carbon-font-defined-sizes
-			    13
-			    carbon-font-encode-family-list-vera)
-
-;; ;; monaco + hiragino maru gothic
-;; (setq carbon-font-encode-family-list-monaco
-;;       '((ascii . "monaco")
-;; 	(japanese-jisx0208 . "ヒラギノ丸ゴ*")
-;; 	(katakana-jisx0201 . "ヒラギノ丸ゴ*")
-;; 	(korean-ksc5601 . "applegothic*")))
-;; (carbon-font-create-fontset "monamaru"
-;; 			    ;; carbon-font-defined-sizes
-;; 			    11
-;; 			    carbon-font-encode-family-list-monaco)
-
-
-
-(setq default-frame-alist (append (list
-				   ;;'(font . "fontset-profontmaru")
-				   ;;'(font . "fontset-verdanamaru")
-				   ;;'(font . "fontset-monamaru")
-				   '(font . "fontset-veramaru")
-				   )default-frame-alist))
 ;;-----------------------------------------------------------------
 ;; Color
 ;;-----------------------------------------------------------------
@@ -81,3 +20,6 @@
 ;; anti-aliasing by Quartz 2D
 ;;-----------------------------------------------------------------
 (setq mac-allow-anti-aliasing t)
+
+;; no beep, no flush
+(setq ring-bell-function 'ignore)
