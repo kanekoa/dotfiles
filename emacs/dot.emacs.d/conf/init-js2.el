91d573864e3a50af2da76a887ad863d3e2397c3b
kanekoa <kanekoa301@gmail.com>
2014-01-17 16:59:52 +0900

    Update js2-mode.el and configuration

--- emacs/dot.emacs.d/conf/init-js2.el
+++ emacs/dot.emacs.d/conf/init-js2.el
@@ -1,12 +1,12 @@
 ;; -*- Mode: Emacs-Lisp ; Coding: utf-8 -*-
 
 ;; js2-mode
-(autoload 'js2-mode "js2" nil t)
+(autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook
           '(lambda ()
-             (setq js2-basic-offset 4)
-             (setq tab-width 4)
+             (setq js2-basic-offset 2)
+             (setq tab-width 2)
              (setq indent-tabs-mode nil)            ;; タブを無効に
              (setq js2-cleanup-whitespace nil)      ;; 行末空白の自動削除無効
              (setq js2-enter-indents-newline nil)   ;; C-mでインデントしないように
